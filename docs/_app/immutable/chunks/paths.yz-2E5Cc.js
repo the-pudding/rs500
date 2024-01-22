2064f2e21c38a3b4582e6f6443dd11563d32823c
Matt <matt@matt.local>
2024-01-22 16:32:49 -0500

    update github pages

---
+++ docs/_app/immutable/chunks/paths.yz-2E5Cc.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_derl90?.base??"",e=globalThis.__sveltekit_derl90?.assets??s;export{e as a,s as b};
---
+++ docs/_app/immutable/entry/start.inXAe6F-.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.SpY6S15W.js";export{t as start};
